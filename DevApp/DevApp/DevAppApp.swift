7fb88a9c2b202f1594907ddad13c62a1813b6ab3
Kojiro Ito <>
2023-12-26 10:42:49 +0900

    init Dev project

---
+++ DevApp/DevApp/DevAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevAppApp.swift
+//  DevApp
+//
+//  Created by Kojiro Ito on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct DevAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
